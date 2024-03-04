b11f6d22a74dd4d17755c040b57051138e75698e
Njaka91 <andriaravelo91@gmail.com>
2024-03-04 11:28:28 +0300

    configuration DB creation model User creation controller User

---
+++ Mihaja/Models/DbConnexionUser.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Mihaja.Models
+{
+    public class DbConnexionUser
+    {
+        public static NpgsqlConnection connectionString =
+            new NpgsqlConnection(ConfigurationManager.ConnectionStrings["todomihaja"].ConnectionString);
+
+    }
+}
---
+++ Mihaja/Models/User.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mihaja.Models
+{
+    public class User
+    {
+        private int _userId;
+        private string _userName;
+        private string _userFirstName;
+        private DateTime _userBirth;
+
+        public int UserId   
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }      
+        public string UserFirstName
+        {
+            get { return _userFirstName; }
+            set { _userFirstName = value; }
+        }
+        public DateTime UserBirth
+        {
+            get { return _userBirth; }
+            set { _userBirth = value; }
+        }
+
+        public User()
+        {
+        }
+
+        public User(int userId, string userName, string userFirstName, DateTime userBirth)
+        {
+            _userId = userId;
+            _userName = userName;
+            _userFirstName = userFirstName;
+            _userBirth = userBirth;
+        }
+
+        public User(string userName, string userFirstName, DateTime userBirth)
+        {
+            _userName = userName;
+            _userFirstName = userFirstName;
+            _userBirth = userBirth;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   _userId == user._userId &&
+                   _userName == user._userName &&
+                   _userFirstName == user._userFirstName &&
+                   _userBirth == user._userBirth;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -99222322;
+            hashCode = hashCode * -1521134295 + _userId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_userName);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_userFirstName);
+            hashCode = hashCode * -1521134295 + _userBirth.GetHashCode();
+            return hashCode;
+        }
+    }
+}
