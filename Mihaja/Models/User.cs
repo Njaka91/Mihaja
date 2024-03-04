10ddd1fdc3c368cf3ca194afc3925f6be275b607
Njaka91 <andriaravelo91@gmail.com>
2024-03-04 16:58:24 +0300

    mihaja 2.0.1

--- Mihaja/Models/User.cs
+++ Mihaja/Models/User.cs
@@ -47,38 +47,21 @@ namespace Mihaja.Models
         {
         }
 
-        public User(int userId, string userName, string userFirstName, DateTime userBirth)
+        public User(int userId, string userName, string userFirstName, DateTime userBirth, string userPassword)
         {
             _userId = userId;
             _userName = userName;
             _userFirstName = userFirstName;
             _userBirth = userBirth;
+            _userPassword = userPassword;
         }
 
-        public User(string userName, string userFirstName, DateTime userBirth)
+        public User(string userName, string userFirstName, DateTime userBirth, string userPassword)
         {
             _userName = userName;
             _userFirstName = userFirstName;
             _userBirth = userBirth;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is User user &&
-                   _userId == user._userId &&
-                   _userName == user._userName &&
-                   _userFirstName == user._userFirstName &&
-                   _userBirth == user._userBirth;
-        }
-
-        public override int GetHashCode()
-        {
-            int hashCode = -99222322;
-            hashCode = hashCode * -1521134295 + _userId.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_userName);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_userFirstName);
-            hashCode = hashCode * -1521134295 + _userBirth.GetHashCode();
-            return hashCode;
+            _userPassword = userPassword;
         }
     }
 }
