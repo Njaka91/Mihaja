c52be711b6f5a8087769502b20168c0a5c98e451
Njaka91 <andriaravelo91@gmail.com>
2024-03-05 13:57:07 +0300

    add task class, taslcontroller task

---
+++ Mihaja/Models/DbConnexionTasks.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Mihaja.Models
+{
+    public class DbConnexionTasks
+    {
+        public static NpgsqlConnection connectionString =
+           new NpgsqlConnection(ConfigurationManager.ConnectionStrings["todomihaja"].ConnectionString);
+
+        public static List<Task> ListeTasks (Task task)
+        {
+            var req = $"SELECT * FROM public.task WHERE taskname = '{task.TaskName}' AND taskuser = '{task.TaskUser}' ";
+            try
+            {
+                connectionString.Open();
+                var cmd = new NpgsqlCommand(req, connectionString);
+                cmd.ExecuteNonQuery();
+                connectionString.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Mihaja/Models/Task.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mihaja.Models
+{
+    public class Task
+    {
+        private int _id;
+        private string _taskName;
+        private string _taskUser;
+        private Boolean _taskStatement;
+
+        public int TaskId
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string TaskName
+        {
+            get { return _taskName; }
+            set { _taskName = value; }
+        }
+        public string TaskUser
+        {
+            get { return _taskUser; }
+            set { _taskUser = value; }
+        }
+        public Boolean TaskStatement
+        {
+            get { return _taskStatement; }
+            set { _taskStatement = value; }
+        }
+
+        public Task()
+        {
+        }
+
+        public Task(int id, string taskName, string taskUser, bool taskStatement)
+        {
+            _id = id;
+            _taskName = taskName;
+            _taskUser = taskUser;
+            _taskStatement = taskStatement;
+        }
+
+        public Task(string taskName, string taskUser, bool taskStatement)
+        {
+            _taskName = taskName;
+            _taskUser = taskUser;
+            _taskStatement = taskStatement;
+        }
+    }
+}
